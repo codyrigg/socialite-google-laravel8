d37c913312a302455d66dbedbf2d1999d1b5297e
Cody Rigg <itsupport@regent-college.edu>
2020-09-21 15:48:42 -0700

    Initial commit

---
+++ app/Http/Controllers/LoginController.php
@@ -0,0 +1,68 @@
+<?php
+
+namespace App\Http\Controllers;
+use App\Http\Controllers\Controller;
+use Laravel\Socialite\Facades\Socialite;
+use Illuminate\Support\Facades\Auth;
+use Exception;
+use App\Models\User;
+use App\Models\Team;
+
+class LoginController extends Controller
+{
+    /**
+     * Redirect the user to the GitHub authentication page.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function redirectToGoogle()
+    {
+        return Socialite::driver('google')->redirect();
+    }
+
+    /**
+     * Obtain the user information from GitHub.
+     *
+     * @return \Illuminate\Contracts\Foundation\Application|\Illuminate\Http\RedirectResponse|\Illuminate\Http\Response|\Illuminate\Routing\Redirector
+     */
+    public function handleGoogleCallback()
+    {
+        try {
+            //create a user using socialite driver google
+            $user = Socialite::driver('google')->user();
+            // if the user exits, use that user and login
+            $finduser = User::where('google_id', $user->id)->first();
+            if($finduser){
+                //if the user exists, login and show dashboard
+                Auth::login($finduser);
+                return redirect('/dashboard');
+            }else{
+                //user is not yet created, so create first
+                $newUser = User::create([
+                    'name' => $user->name,
+                    'email' => $user->email,
+                    'google_id'=> $user->id,
+                    'password' => encrypt('')
+                ]);
+                //every user needs a team for dashboard/jetstream to work.
+                //create a personal team for the user
+                $newTeam = Team::forceCreate([
+                    'user_id' => $newUser->id,
+                    'name' => explode(' ', $user->name, 2)[0]."'s Team",
+                    'personal_team' => true,
+                ]);
+                // save the team and add the team to the user.
+                $newTeam->save();
+                $newUser->current_team_id = $newTeam->id;
+                $newUser->save();
+                //login as the new user
+                Auth::login($newUser);
+                // go to the dashboard
+                return redirect('/dashboard');
+            }
+            //catch exceptions
+        } catch (Exception $e) {
+            dd($e->getMessage());
+        }
+    }
+}
---
+++ routes/web.php
@@ -0,0 +1,25 @@
+<?php
+
+use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\LoginController;
+
+/*
+|--------------------------------------------------------------------------
+| Web Routes
+|--------------------------------------------------------------------------
+|
+| Here is where you can register web routes for your application. These
+| routes are loaded by the RouteServiceProvider within a group which
+| contains the "web" middleware group. Now create something great!
+|
+*/
+
+Route::get('/', function () {
+    return view('welcome');
+});
+Route::get('auth/google', [LoginController::class, 'redirectToGoogle']);
+Route::get('auth/google/callback', [LoginController::class, 'handleGoogleCallback']);
+
+Route::middleware(['auth:sanctum', 'verified'])->get('/dashboard', function () {
+    return view('dashboard');
+})->name('dashboard');
